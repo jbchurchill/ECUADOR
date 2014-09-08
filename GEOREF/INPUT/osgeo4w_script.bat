87a48d4c61d2c1c523733f27f4508cae2e164ede
JB <jchurchi@gmail.com>
2014-09-08 18:54:56 -0400

    added entire GEOREF folder from stuff I did during lunch today.

---
+++ GEOREF/INPUT/osgeo4w_script.bat
@@ -0,0 +1,6 @@
+for f in *.tiff
+do
+  echo "Processing $f"
+  gdal_translate -of GTiff -gcp 2.01299 182.543 498646 9.75808e+06 -gcp 293.113 83.5611 1.13967e+06 9.98832e+06 -gcp 117.885 2.24701 736633 1.01607e+07 -gcp 142.97 253.152 812548 9.61267e+06 -gcp 34.0952 284.723 576335 9.53561e+06 -gcp 8.80097 121.934 510282 9.88275e+06 $f "C:\Users\jchurchill\TMP_WORK\TMP_BIRDSEYE_ADDENDUM\TMP\\$f"
+  #gdalwarp -r near -order 1 -co COMPRESS=NONE  "C:\Users\jchurchill\TMP_WORK\TMP_BIRDSEYE_ADDENDUM\\TMP\\$f" "C:\Users\jchurchill\TMP_WORK\TMP_BIRDSEYE_ADDENDUM\OUTPUT\\$f"
+done
