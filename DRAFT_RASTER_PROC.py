9f9b84f0698c8b44431bdd8bbccd2e366aaaaf6c
J.B. Churchill <jchurchill@garrettcounty.org>
2014-09-29 13:05:42 -0400

    moved some files around INPUT and OUTPUT for the batch scripts.

--- DRAFT_RASTER_PROC.py
+++ DRAFT_RASTER_PROC.py
@@ -1,11 +1,7 @@
-# Import system modules
-# import sys, string, os, time, arcpy
 import arcpy
-# is this next part necessary ?
 from arcpy.sa import *
 arcpy.CheckOutExtension("spatial")
-#arcpy.env.workspace = r"C:\Users\jchurchill\TMP_WORK\TMP_BIRDSEYE\ECUADOR\ws2"
-outws = r"C:\Users\jchurchill\TMP_WORK\TMP_BIRDSEYE\ECUADOR\ws3"
+outws = r"C:\Users\jchurchill\TMP_WORK\TMP_BIRDSEYE\ECUADOR\ws4"
 inws = r"C:\Users\jchurchill\TMP_WORK\TMP_BIRDSEYE\ECUADOR\GEOREF\OUTPUT_RENAMED"
 theMask = r"C:\Users\jchurchill\TMP_WORK\TMP_BIRDSEYE\ECUADOR\ws2\mask01"
 arcpy.env.workspace = inws
@@ -18,20 +14,15 @@ try:
     #testing
     print raster
     ConResult = Con(Raster(raster) < 255, Con(Raster(theMask) == 1, 1, 0), 0)
-    for x in range(0, 3):
-      outFM = "FM" + str(raster[:-6]) + str(x)
-      outCon = str(raster[:-6]) + str(x)
+    for x in range(0, 4):
+      outFM = "FM" + str(raster[:-5]) + str(x)
+      outCon = str(raster[:-5]) + str(x)
       # RUN FOCAL MAJORITY
       FMresult = FocalStatistics(ConResult, neighborhood, "MAJORITY", "")
-      ### Do I need to save it ?????
-      # FMresult.save(outws + "\\" + outFM)
-      ### Do I need to save it ?????
-      # FMresult = ?????????
       # Con (in_conditional_raster, in_true_raster_or_constant, {in_false_raster_or_constant}, {where_clause})
       ConResult2 = Con(IsNull(FMresult), ConResult, FMresult)
-      ConResult2.save(outws + "\\" + outCon)
-      # CITI03B = Con(IsNull("CITI02F5"), "CITI02", "CITI02F5")
-      # RUN CON
+      if (x == 4):
+        ConResult2.save(outws + "\\" + outCon)
 
 except:
     # If an error occurred print the message to the screen
