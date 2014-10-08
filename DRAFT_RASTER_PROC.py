9a6b9635a54bfeeafeb6f684b6eeaad8eeeb9546
J.B. Churchill <jchurchill@garrettcounty.org>
2014-10-08 12:42:47 -0400

    re-ran ws4 output (Raster Proc) with 6x filtering.

--- DRAFT_RASTER_PROC.py
+++ DRAFT_RASTER_PROC.py
@@ -3,7 +3,7 @@ from arcpy.sa import *
 arcpy.CheckOutExtension("spatial")
 outws = r"C:\Users\jchurchill\TMP_WORK\TMP_BIRDSEYE\ECUADOR\ws4"
 inws = r"C:\Users\jchurchill\TMP_WORK\TMP_BIRDSEYE\ECUADOR\GEOREF\OUTPUT_655"
-theMask = r"C:\Users\jchurchill\TMP_WORK\TMP_BIRDSEYE\ECUADOR\ws2\mask02"
+theMask = r"C:\Users\jchurchill\TMP_WORK\TMP_BIRDSEYE\ECUADOR\ws2\mask03"
 # was mask01 in ws3
 arcpy.env.workspace = inws
 
@@ -15,14 +15,14 @@ try:
     #testing
     print raster
     ConResult = Con(Raster(raster) < 255, Con(Raster(theMask) == 1, 1, 0), 0)
-    for x in range(0, 4):
+    for x in range(0, 6):
       outFM = "FM" + str(raster[:-5]) + str(x)
       outCon = str(raster[:-5]) + str(x)
       # RUN FOCAL MAJORITY
       FMresult = FocalStatistics(ConResult, neighborhood, "MAJORITY", "")
       # Con (in_conditional_raster, in_true_raster_or_constant, {in_false_raster_or_constant}, {where_clause})
       ConResult2 = Con(IsNull(FMresult), ConResult, FMresult)
-      if (x == 3):
+      if (x == 5):
         ConResult2.save(outws + "\\" + outCon)
 
 except:
